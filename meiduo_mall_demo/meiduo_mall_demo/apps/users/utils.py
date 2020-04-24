797c2e94b5b204f310c4103bff62f7a5ddb3c3fa
lhcc <lhcc1020@163.com>
2020-04-24 20:14:01 +0800

    手机号登录接口完成

---
+++ meiduo_mall_demo/meiduo_mall_demo/apps/users/utils.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.backends import ModelBackend
+import re
+from .models import User
+
+
+def checkusername(account):
+    '''判断accout是手机号还是用户名'''
+    # 操作数据库放在try中
+    try:
+        if re.match(r'^1[3-9]\d{9}$', account):
+            user = User.objects.get(mobile = account)
+        else:
+            user = User.objects.get(username=account)
+    except Exception as e:
+        return None
+
+    else:
+        return user
+
+
+class UsernameCheck(ModelBackend):
+    """自定义用户认证后端,继承ModelBackend"""
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        # 重写继承的认证方法
+        user = checkusername(username)
+
+        # 确认判断结果,并校验密码
+        if user and user.check_password(password):
+            return user
--- meiduo_mall_demo/meiduo_mall_demo/settings/dev.py
+++ meiduo_mall_demo/meiduo_mall_demo/settings/dev.py
@@ -214,3 +214,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://www.meiduo.site:8080',
 )
 CORS_ALLOW_CREDENTIALS = True  # 允许携带cookie
+
+
+# 指定自定义用户认证后端
+AUTHENTICATION_BACKENDS = ['users.utils.UsernameCheck']
